5c7402447f73affdcc561a3a77096408389dd6d1
Devadathan K S <devadathanks04@gmail.com>
2026-08-20 15:31:50 +0530

    Added Lambda and APIGateway endpoint

--- lib/ci-cd-aws-pipeline-demo-stack.ts
+++ lib/ci-cd-aws-pipeline-demo-stack.ts
@@ -1,13 +1,14 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_lambda, aws_apigateway } from 'aws-cdk-lib';
+import path from 'path';
 
 export class CiCdAwsPipelineDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'Pipeline', {
+    const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'TestPipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.connection("itsdks/CI-CD-AWS-Pipeline-Demo", "main", {
@@ -15,6 +16,18 @@ export class CiCdAwsPipelineDemoStack extends cdk.Stack {
         }),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
+    });
+
+    const lambdaFn = new aws_lambda.Function(this, 'MyLambda', {
+      functionName: 'MyLambdaFunction',
+      runtime: aws_lambda.Runtime.NODEJS_LATEST,
+      handler: 'index.handler',
+      code: aws_lambda.Code.fromAsset(path.join(__dirname, 'lambda'))
+    });
+
+    const endpoint = new aws_apigateway.LambdaRestApi(this, 'MyEndpoint', {
+      handler: lambdaFn,
+      restApiName: 'MyNewEndpoint'
     });
   }
 }
